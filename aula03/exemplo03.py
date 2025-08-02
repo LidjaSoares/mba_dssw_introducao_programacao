d629be67127f6aefd87e3bb281552b2b00f23e32
Lidja Soares <lidjareginasoares@gmail.com>
2025-08-02 14:20:52 -0300

    Atualização da aula 02

---
+++ aula03/exemplo03.py
@@ -0,0 +1,13 @@
+posgraduandos = [
+    'Geiseane',
+    'Eduardo',
+    'MariaAugusta',
+    'George',
+    'Newton',
+    'Raysse',
+    'Isabela'
+]
+
+mensagem = ', venha para minha festa de aniversário, mas traga sua comida e meu vinho! (Att. Agda)'
+for nome in posgraduandos:
+        print(nome + mensagem)
---
+++ listadetarefas.py
@@ -0,0 +1,9 @@
+listasdetarefas = [
+    'Treino', 'Banho', 'Estudo', 'Organização da casa', 'Almoco', 'Trabalho'
+
+]
+
+mensagem = ', Feito!'
+
+for nome in listasdetarefas:
+    print(nome + mensagem)
